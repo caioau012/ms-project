dfc42f180d849e940fb817eeec2f400d08e60ba0
caioau012 <caio_hudak@hotmail.com>
2025-08-03 10:34:50 -0300

    hr-worker implementation

--- hr-worker/src/main/java/com/caiohudak/hrworker/HrWorkerApplication.java
+++ hr-worker/src/main/java/com/caiohudak/hrworker/HrWorkerApplication.java
@@ -1,4 +1,4 @@
-package com.caiohudak.htworker;
+package com.caiohudak.hrworker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
