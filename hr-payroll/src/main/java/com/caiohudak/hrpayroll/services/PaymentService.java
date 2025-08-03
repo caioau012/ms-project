2f355287281bb2a27d6d81d11639a6a61eb2975f
caioau012 <caio_hudak@hotmail.com>
2025-08-03 15:55:01 -0300

    hr-payroll implementation mock

---
+++ hr-payroll/src/main/java/com/caiohudak/hrpayroll/services/PaymentService.java
@@ -0,0 +1,13 @@
+package com.caiohudak.hrpayroll.services;
+
+import org.springframework.stereotype.Service;
+
+import com.caiohudak.hrpayroll.entities.Payment;
+
+@Service
+public class PaymentService {
+	
+	public Payment getPayment(long workerId, int days) {
+		return new Payment("Bob", 200.0, days);
+	}
+}
