60db078be7e55c42475a42100ee25b52b1a585f1
caioau012 <caio_hudak@hotmail.com>
2025-08-03 16:29:15 -0300

    Ribbon load balancing

--- hr-payroll/src/main/java/com/caiohudak/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/caiohudak/hrpayroll/services/PaymentService.java
@@ -1,10 +1,7 @@
 package com.caiohudak.hrpayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.caiohudak.hrpayroll.entities.Payment;
