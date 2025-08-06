6451667273523bcf25bd062fb1fa4c3b38d6f966
caioau012 <caio_hudak@hotmail.com>
2025-08-05 21:15:48 -0300

    Database seeding

--- hr-user/src/main/java/com/caiohudak/hruser/HrUserApplication.java
+++ hr-user/src/main/java/com/caiohudak/hruser/HrUserApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HrUserApplication {
+public class HrUserApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrUserApplication.class, args);
