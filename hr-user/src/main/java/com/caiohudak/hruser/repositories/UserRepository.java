be6196ba9b847b98aedea1afbcbd5a10bd3b5b00
caioau012 <caio_hudak@hotmail.com>
2025-08-05 21:28:45 -0300

    UserRepository, UserResource, Zuul config

---
+++ hr-user/src/main/java/com/caiohudak/hruser/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.caiohudak.hruser.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.caiohudak.hruser.entites.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+	User findByEmail(String email);
+	
+}
