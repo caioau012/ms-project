73dd1bfffe795aa1774678c9293de34f02c1fdb9
caioau012 <caio_hudak@hotmail.com>
2025-08-06 22:23:09 -0300

    Cors configuration

--- hr-api-gateway-zuul/src/main/java/com/caiohudak/hrapigatewayzuul/config/ResourceServerConfig.java
+++ hr-api-gateway-zuul/src/main/java/com/caiohudak/hrapigatewayzuul/config/ResourceServerConfig.java
@@ -1,14 +1,21 @@
 package com.caiohudak.hrapigatewayzuul.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
-
-import com.netflix.ribbon.proxy.annotation.Http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableResourceServer
@@ -28,11 +35,33 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 		resources.tokenStore(tokenStore);
 	}
 
+	@SuppressWarnings("removal")
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		
 		http.authorizeHttpRequests(auth -> auth.requestMatchers(PUBLIC).permitAll().requestMatchers(OPERATOR).hasAnyRole("OPERATOR", "ADMIN").requestMatchers(ADMIN).hasRole("ADMIN").anyRequest().authenticated());
 		
+		http.cors().configurationSource(corsConfigurationSource());
+		
 	}
-
+	
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration corsConfig = new CorsConfiguration();
+		corsConfig.setAllowedOrigins(Arrays.asList("*"));
+		corsConfig.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "PATCH"));
+		corsConfig.setAllowCredentials(true);
+		corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+		
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", corsConfig);
+		return source;
+	}
+	
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter(){
+		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(corsConfigurationSource()));
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return bean;
+	}
 }
